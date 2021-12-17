129299880d727a06be5bfa27e766c661aab01dfc
zmantas17 <mantas.zemaitis.0427@stud.kitm.lt>
2021-12-17 23:21:20 +0200

    changed design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
-                <h4>Jūsų užregistruoti renginiai</h4>
+                
+                <h4 class="text-center py-3">Jūsų užregistruoti renginiai</h4>
                 <table class="min-w-full divide-y divide-gray-200">
                 <thead class="bg-gray-50">
                     <tr>
